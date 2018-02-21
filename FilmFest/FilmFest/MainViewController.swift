b8f106e9691de12a11fa1ab3286e56872f2f894d
fanny <fc2@cin.ufpe.br>
2018-02-21 19:52:32 -0300

    Adding base project

---
+++ FilmFest/FilmFest/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FilmFest
+//
+//  Created by Fanny Chien on 21/02/18.
+//  Copyright © 2018 Funnyfanny. All rights reserved.
+//
+
+import UIKit
+import ChameleonFramework
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //self.view.backgroundColor = GradientColor()
+        
+        
+    }
+
+
+}
+
